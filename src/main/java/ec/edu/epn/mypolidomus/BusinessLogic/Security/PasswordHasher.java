f1ac09f900f83e85c239efb2803b38ebb9c9e123
C-b4s <szuniga046@gmail.com>
2026-01-29 10:23:05 -0500

    Se realizaron cambios en los controllers y se añadieron clases en la Business Logic

---
+++ src/main/java/ec/edu/epn/mypolidomus/BusinessLogic/Security/PasswordHasher.java
@@ -0,0 +1,21 @@
+package ec.edu.epn.mypolidomus.BusinessLogic.Security;
+
+import java.security.MessageDigest;
+
+public class PasswordHasher {
+    public static String hash(String password) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            byte[] bytes = md.digest(password.getBytes());
+
+            StringBuilder sb = new StringBuilder();
+            for (byte b : bytes) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+
+        } catch (Exception e) {
+            throw new RuntimeException("Error al hashear contraseña", e);
+        }
+    }
+}
